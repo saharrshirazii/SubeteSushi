e9ff2973cba991b994a5f213276c239244319bfa
saharrshirazii <saharr.shirazii@gmail.com>
2025-12-10 17:06:27 +0100

    updates

--- js/components/menu.js
+++ js/components/menu.js
@@ -19,7 +19,7 @@ const recipes = [
     category: "Nigiri and Maki",
     price: 260,
     img: "img/20PieceSushi.jpg",
-    desc: `4 salmon, 2 shirimp, 1 tuna, 1 gunkan, 1 unagi, 1 trout, 10 special maki rolls.`
+    desc: `4 salmon, 2 shirimp, 1 tuna, 1 gunkan, 1 unagi, 1 trout, 10 special maki rolls`
   },
   {
     id: 2,
@@ -27,7 +27,7 @@ const recipes = [
     category: "Nigiri and Maki",
     price: 395,
     img: "img/30PieceFamilySushi.jpg",
-    desc: `6 salmon, 2 shrimp, 2 tuna, 1 gunkan, 1 black fish, 2 unagi, 1 white fish, 10 special maki rolls, 5 maki rolls.`
+    desc: `6 salmon, 2 shrimp, 2 tuna, 1 gunkan, 1 black fish, 2 unagi, 1 white fish, 10 special maki rolls, 5 maki rolls`
   },
   {
     id: 3,
@@ -35,7 +35,7 @@ const recipes = [
     category: "Nigiri and Maki",
     price: 120,
     img: "img/9PieceSushi.jpg",
-    desc: `2 salmon, 1 shrimp, 1 tuna, 1 white fish and 4 maki rolls.`
+    desc: `2 salmon, 1 shrimp, 1 tuna, 1 white fish and 4 maki rolls`
   },
   {
     id: 4,
@@ -43,7 +43,7 @@ const recipes = [
     category: "Nigiri and Maki",
     price: 145,
     img: "img/11PieceSushi.jpg",
-    desc: `3 salmon, 1 shrimp, 1 tuna, 1 white fish, 1 unagi and 4 maki rolls.`
+    desc: `3 salmon, 1 shrimp, 1 tuna, 1 white fish, 1 unagi and 4 maki rolls`
   },
   {
     id: 5,
@@ -51,7 +51,7 @@ const recipes = [
     category: "Nigiri and Maki",
     price: 180,
     img: "img/13PieceSushi.jpg",
-    desc: `3 salmon, 1 shrimp, 1 tuna, 1 gunkan, 1 unagi, 1 trout and 5 maki rolls.`
+    desc: `3 salmon, 1 shrimp, 1 tuna, 1 gunkan, 1 unagi, 1 trout and 5 maki rolls`
   },
   {
     id: 6,
@@ -59,14 +59,14 @@ const recipes = [
     category: "Nigiri and Maki",
     price: 140,
     img: "img/Vegetarian11PieceSushi.jpg",
-    desc: `2 avocado, 1 bell pepper, 2 tofu, 1 gunkan, 5 maki rolls.`
+    desc: `2 avocado, 1 bell pepper, 2 tofu, 1 gunkan, 5 maki rolls`
   },
   { id: 7,
     title: "Vegetarian 9 Piece Sushi",
     category: "Nigiri and Maki",
     price: 120,
     img: "img/Vegetarian9PieceSushi.jpg",
-    desc: `2 avocado, 1 bell pepper, 2 tofu and 4 maki rolls.`
+    desc: `2 avocado, 1 bell pepper, 2 tofu and 4 maki rolls`
   },
   {
     id: 8,
@@ -74,7 +74,7 @@ const recipes = [
     category: "Nigiri and Maki",
     price: 190,
     img: "img/15PieceSushi.jpg",
-    desc: `4 salmon, 2 shirimp, 1 tuna, 1 gunkan, 1 unagi, 1 trout, 5 maki rolls.`
+    desc: `4 salmon, 2 shirimp, 1 tuna, 1 gunkan, 1 unagi, 1 trout, 5 maki rolls`
   },
 
   {
@@ -83,7 +83,7 @@ const recipes = [
     category: "Nigiri/ Flambeed Nigiri",
     price: 160,
     img: "img/10PieceSearedSalmon.jpg",
-    desc: `Lightly grilled salmon and chili mayonnaise.`
+    desc: `Lightly grilled salmon and chili mayonnaise`
   },
 
   {
@@ -101,7 +101,7 @@ const recipes = [
     category: "Nigiri/ Flambeed Nigiri",
     price: 150,
     img: "img/10PieceNigiri.jpg",
-    desc: `5 salmon, 5 avocado.`
+    desc: `5 salmon, 5 avocado`
   },
 
   {
@@ -110,7 +110,7 @@ const recipes = [
     category: "Nigiri/ Flambeed Nigiri",
     price: 150,
     img: "img/10PieceNigiri2.jpg",
-    desc: `10 salmon.`
+    desc: `10 salmon`
   },
 
   {
@@ -128,7 +128,7 @@ const recipes = [
     category: "Appetizers",
     price: 65,
     img: "img/Temaki.jpg",
-    desc: `Fried shrimp/Tuna/Salmon/Vegetarian, Avocado, Chives, Cucumber.`
+    desc: `Fried shrimp/Tuna/Salmon/Vegetarian, Avocado, Chives, Cucumber`
   },
 
   {
@@ -166,7 +166,133 @@ const recipes = [
     img: "img/Edamame.jpg",
     desc: ``
   },
-  
+
+  {
+    id: 19,
+    title: "Vegan/Vegetarian Roll",
+    category: "Special Makimono Rolls",
+    price: 165,
+    img: "img/Vegansk-VegetariskRoll.jpg",
+    desc: `Cucumber, Mango, Pickled red onion, Avocado`
+  },
+
+  {
+    id: 20,
+    title: "Spicy Salmon Roll",
+    category: "Special Makimono Rolls",
+    price: 190,
+    img: "img/SpicySalmonRoll.jpg",
+    desc: `Salmon, Avocado, Cucumber, Mango, Pickled red onion, Spicy sauce`
+  },
+
+  {
+    id: 21,
+    title: "Spicy Tuna Roll",
+    category: "Special Makimono Rolls",
+    price: 180,
+    img: "img/SpicyTunaRoll.jpg",
+    desc: `Tuna, Avocado, cucumber, Mango, Pickled red onion, Spicy sauce`
+  },  
+
+  {
+    id: 22,
+    title: "Tempura Roll",
+    category: "Special Makimono Rolls",
+    price: 180,
+    img: "img/TempuraRoll.jpg",
+    desc: `Avocado, Cucumber, Chives, Salad, Fried shrimp`
+  },
+
+  {
+    id: 23,
+    title: "Volcano Fried Roll",
+    category: "Special Makimono Rolls",
+    price: 170,
+    img: "img/VolcanoFriedRoll.jpg",
+    desc: `Cucumber, Salad, Spring onion, Avocado, Spicy salmon, Chili mayonnaise`
+  },
+
+  {
+    id: 24,
+    title: "Philadelphia Roll",
+    category: "Special Makimono Rolls",
+    price: 170,
+    img: "img/PhiladephiaRoll.jpg",
+    desc: `Avocado, Cucumber, Salad, Lighty seared salmon on top, Cream cheese`
+  },
+
+  {
+    id: 25,
+    title: "California Roll",
+    category: "Special Makimono Rolls",
+    price: 160,
+    img: "img/CaliforniaRoll.jpg",
+    desc: `Avocado, Cucumber, Crab, Japanese mayonnaise`
+  },
+  {
+    id: 26,
+    title: "Marinated Salmon/Tuna/Karaage",
+    category: "Subete Poke Bowl",
+    price: 175,
+    img: "img/MarinatedSalmon-Tuna-Karaage.jpg",
+    desc: `Avocado, Pickled red oninon, Rice, Mango, Edamame, Wakame, Gari, Salad mix with dressing`
+  },
+  {
+    id: 27,
+    title: "Vegan/ Vegetarian",
+    category: "Subete Poke Bowl",
+    price: 165,
+    img: "img/MarinatedSalmon-Tuna-Karaage.jpg",
+    desc: `Marinated tofu/ Fried tofu, Rice, Avocado, Wakame, Gari, Pickled red onion, Salad and Mango`
+  },
+  {
+    id: 28,
+    title: "Salmon Teryaki",
+    category: "Japanese Hot Dishes",
+    price: 170,
+    img: "img/SalmonTeriyaki.jpg",
+    desc: `Fried salmon, Rice, Salad mix with dressing`
+  },
+  {
+    id: 29,
+    title: "Yakinuki",
+    category: "Japanese Hot Dishes",
+    price: 170,
+    img: "img/Yakinuki.jpg",
+    desc: `Sliced beef, Rice, Salad mix with dressing`
+  },
+  {
+    id: 30,
+    title: "Yakitori",
+    category: "Japanese Hot Dishes",
+    price: 160,
+    img: "img/Yakitori.jpg",
+    desc: `Chiken skewers, Rice, Salad mix with dressing`
+  },
+  {
+    id: 31,
+    title: "Dumplings",
+    category: "Japanese Hot Dishes",
+    price: 160,
+    img: "img/Dumplings.jpg",
+    desc: `Vegan/ Vegetarian/ Chiken/ Shrimp, Rice, Salad mix with dressing`
+  },
+  {
+    id: 32,
+    title: "Karaage",
+    category: "Japanese Hot Dishes",
+    price: 170,
+    img: "img/Karaage.jpg",
+    desc: `Marinated, Fried chiken with panko, Rice, Salad mix with dressing`
+  },
+  {
+    id: 33,
+    title: "Bento Box",
+    category: "Japanese Hot Dishes",
+    price: 190,
+    img: "img/Karaage.jpg",
+    desc: `Salmon/ Chicken Skewers/ Beef/ Karaage,  3 pieces of nigiri, Rice, Salad mix with dressing`
+  },
 ]
 
 const sectionCenter = document.querySelector(".section-center");
@@ -203,7 +329,7 @@ function displayMenuItems(menuItems){
   let displayMenu = menuItems.map(function(item){
     //console.log(item);
     return `<article class="menu-item">
-          <img src=${item.img} alt=${item.title} class="photo" />
+          <img src=${item.img} alt=${item.title} class="photo" loading="lazy"/>
           <div class="item-info">
             <div>
               <h4>${item.title}</h4>
