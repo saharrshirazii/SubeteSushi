9f408dfb72b641fb4aec86b37e149eb367cd9545
saharrshirazii <saharr.shirazii@gmail.com>
2026-01-02 12:48:40 +0100

    I deleted files with old prefix

--- js/components/toggle-old.js
+++
@@ -1,8 +0,0 @@
-export function toggleMenu(){
-    const navToggle = document.querySelector('.nav-toggle');
-    const links = document.querySelector('.menu-links');
-
-    navToggle.addEventListener ('click', function(){
-    links.classList.toggle('show-links');
-})
-}
